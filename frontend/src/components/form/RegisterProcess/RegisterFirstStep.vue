dd0c4c187ad7c3ea8afc46cbe1041c6f8f7d1eb2
LarsKta <lars.kra2001@gmail.com>
2021-12-05 17:42:07 +0100

    filled with content

--- frontend/src/components/form/RegisterProcess/RegisterFirstStep.vue
+++ frontend/src/components/form/RegisterProcess/RegisterFirstStep.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="register">
-    <h1>This is the Register page 1</h1>
-  </div>
+  <CardTemplate title="Account erstellen" subTitle="Die bundesapp ist ein Service des Deutsche Pfadfinder Bund Mosaik">
+    <TextInput textInputLabel="Vorname" validationError="Name enthält unerlaubte Zeichen" placeholder="Lara" />
+    <br>
+    <TextInput textInputLabel="Nachname" placeholder="Musterfrau"/>
+    <br>
+    <TextInput textInputLabel="Fahrtenname" placeholder="Musti"/>
+    <br>
+    <TextInput textInputLabel="Email" validationError="Email enthält unerlaubte zeichen" placeholder="beispiel@example.com" inputType="email"/>
+  </CardTemplate>
 </template>
 
 <script>
+import TextInput from "./RegisterComponents/TextInput.vue"
+import CardTemplate from "./RegisterComponents/CardTemplate.vue"
+
 export default {
   data() {
     return {
@@ -13,6 +22,10 @@ export default {
       scoutname: '',
       email: ''
     }
+  },
+  components: {
+    TextInput,
+    CardTemplate,
   }
 }
 </script>
