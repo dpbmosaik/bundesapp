7a6cdb94d61c4e467318bbacfe67684c767f7a6d
Tobias Michaely <t_michaely@web.de>
2021-12-05 18:02:06 +0100

    added app navigation

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -35,6 +35,49 @@ const routes = [
     name: 'Demo',
     component: () => import(/* webpackChunkName: "demo" */ '../views/Demo.vue')
   },
+  {
+    path: '/app',
+    name: 'App',
+    component: () => import(/* webpackChunkName: "main-app" */ '../views/Main.vue'),
+    redirect: { name: 'Dashboard' },
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import(/* webpackChunkName: "dashboard" */ '../components/app/Dashboard.vue')
+      },
+      {
+        path: 'mitglieder',
+        name: 'Mitgliederverwaltung',
+        component: () => import(/* webpackChunkName: "mitglieder" */ '../components/app/Members.vue')
+      },
+      {
+        path: 'gruppen',
+        name: 'Gruppenverwaltung',
+        component: () => import(/* webpackChunkName: "Gruppen" */ '../components/app/Groups.vue')
+      },
+      {
+        path: 'verwaltung',
+        name: 'Verwaltung',
+        component: () => import(/* webpackChunkName: "Verwaltung" */ '../components/app/Management.vue')
+      },
+      {
+        path: 'analytics',
+        name: 'Analytics',
+        component: () => import(/* webpackChunkName: "Analytics" */ '../components/app/Analytics.vue')
+      },
+      {
+        path: 'profil',
+        name: 'Profil',
+        component: () => import(/* webpackChunkName: "profil" */ '../components/app/Profile.vue')
+      },
+      {
+        path: 'einstellungen',
+        name: 'Einstellungen',
+        component: () => import(/* webpackChunkName: "einstellungen" */ '../components/app/Settings.vue')
+      },
+    ]
+  },
   // Separated into two routes so that you can also programmatically
   // direct the user to the /404 if missing some data, etc.
   { path: '/404', component: () => import(/* webpackChunkName: "NotFound" */ '../views/NotFound.vue') },
