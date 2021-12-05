d4e5c34aa07525b79dfa4090c083009c3fb2b4ae
LarsKta <lars.kra2001@gmail.com>
2021-12-05 17:31:50 +0100

    added textInput component

---
+++ frontend/src/components/form/textInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <label type="textInput"> {{inputText}}
+            <input for="textInput" type="text">
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "inputText",
+    props: {
+        "inputText": String,
+    }
+};
+</script>
