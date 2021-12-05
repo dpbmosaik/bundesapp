b9e0325b0c8c4658048ba0f5a3f70093ebcdffcb
Tobias Michaely <t_michaely@web.de>
2021-12-05 10:55:47 +0100

    added router logic and state for register process

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    register: {
+      firstname: '',
+      lastname: '',
+      scoutname: '',
+      email: '',
+      stamm: '',
+      group: '',
+      birthdate: '',
+      gender: '',
+      street: '',
+      zipCode: '',
+      additional: '',
+      phone: '',
+      password: ''
+    }
+
   },
   mutations: {
   },
