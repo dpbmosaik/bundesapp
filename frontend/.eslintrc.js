7cbd5201df1eda630d236785345ff61d41387f8f
Robert Bagdahn <rob@vnr.de>
2021-12-12 17:11:06 +0100

    wip

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    "prettier"
-  ],
-  rules: {},
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    env: {
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {},
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
 };
