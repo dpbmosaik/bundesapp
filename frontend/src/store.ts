0f4405dbd0cc3e4c0432865536371c71bd2d7b70
thealeinjo <thea.john@yahoo.de>
2022-04-30 20:52:46 +0200

    first commit

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,4 +1,17 @@
 import { defineStore } from 'pinia'
+import axios from 'axios';
+
+type ScoutOrgaLevel = {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  description: string;
+};
+
+type GetScoutOrgaLevelsResponse = {
+  data: ScoutOrgaLevel[];
+};
 
 const versionString =
   import.meta.env.MODE === 'development'
@@ -10,7 +23,7 @@ export const useStore = defineStore('main', {
     debug: import.meta.env.MODE === 'development',
     version: versionString,
     isInitialized: false,
-    count: 0,
+    scoutOrgaLevels: [] as ScoutOrgaLevel[],
   }),
 
   actions: {
@@ -18,15 +31,42 @@ export const useStore = defineStore('main', {
       this.isInitialized = true
       console.log('app initialized!')
     },
+    async setScoutOrgaLevels() {
+      try {
+        const { data, status } = await axios.get<GetScoutOrgaLevelsResponse>(
+          import.meta.env.VITE_APP_PATH + '/auth/scout-orga-level/',
+          {
+            headers: {
+              Accept: 'application/json',
+            },
+          },
+        );
+    
+        console.log(JSON.stringify(data, null, 4));
+    
+        // 👇️ "response status is: 200"
+        console.log('response status is: ', status);
 
-    increment(value = 1) {
-      this.count += value
-    },
+        this.scoutOrgaLevels = JSON.parse(JSON.stringify(data, null, 4));
+        return data;
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.log('error message: ', error.message);
+          return error.message;
+        } else {
+          console.log('unexpected error: ', error);
+          return 'An unexpected error occurred';
+        }
+      }
+    }
   },
 
   getters: {
     isReady: (state) => {
       return !state.isInitialized
     },
+    getScoutOrgaLevels: (state) => {
+      return state.scoutOrgaLevels
+    }
   },
 })
