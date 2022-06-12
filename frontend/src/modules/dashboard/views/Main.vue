bf198ebcca0c59553a83ff7717bcf8365bd61ae4
Robert Bagdahn <robertbaggi@gmail.com>
2022-06-12 14:56:27 +0200

    cleaned up repo

--- frontend/src/modules/dashboard/views/Main.vue
+++ frontend/src/modules/dashboard/views/Main.vue
@@ -1,13 +1,35 @@
 <template>
-  <h1>Dashboard</h1>
+  <div class="float-left max-h-screen">
+    <UserListVue @user-focus-change="userFocusChange" />
+  </div>
+
+  <div class="w-1/3 float-right">
+    <DashboardSideview :focused-users="focusedUsers" />
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import UserListVue from "@/components/dashboard/UserList.vue";
+import DashboardSideview from "./DashboardSideview.vue";
 
-<script>
-export default {
-
-}
+export default defineComponent({
+  components: {
+    UserListVue,
+    DashboardSideview,
+  },
+  data() {
+    return {
+      focusedUsers: [] as string[],
+    };
+  },
+  methods: {
+    userFocusChange(userId: string) {
+      if (this.focusedUsers.includes(userId)) {
+        this.focusedUsers.splice(this.focusedUsers.indexOf(userId), 1);
+      } else {
+        this.focusedUsers.push(userId);
+      }
+    },
+  },
+});
 </script>
-
-<style>
-
-</style>
