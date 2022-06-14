68f59f4a8e1b731a17db491b7fa9487b6e146356
amarsupial <t_michaely@web.de>
2022-06-14 15:26:54 +0300

    added placeholder dashboard

--- frontend/src/modules/app/views/dashboard/Main.vue
+++ frontend/src/modules/app/views/dashboard/Main.vue
@@ -1,6 +1,13 @@
 <template lang="">
-  <div>
-    Dashboard
+  <div class="grid grid-cols-4 grid-rows-4 h-full gap-8 grid-flow-col">
+    <div class="grid grid-cols-4 grid-rows-4 h-full gap-8 grid-flow-row col-span-2 row-span-4">
+      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-4 row-span-2"></div>
+    </div>
+    <div class="bg-proto-lightgrey rounded-lg col-span-2 row-span-4"></div>
   </div>
 </template>
 <script lang="ts">
