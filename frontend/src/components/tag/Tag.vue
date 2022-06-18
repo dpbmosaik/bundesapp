0e75d5caf6d4dd4401686e6670505e87a05e13ec
amarsupial <t_michaely@web.de>
2022-06-18 15:01:15 +0300

    Added Tag Component

---
+++ frontend/src/components/tag/Tag.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div class="bg-proto-darkgrey text-white px-2 py-1 w-fit h-fit rounded-full font-description">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    
+}
+</script>
