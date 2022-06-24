0ffd6b2143cf5a6650bec366e0659db1ad965e38
amarsupial <t_michaely@web.de>
2022-06-24 10:01:57 +0300

    added titles to dashboard boxes

--- frontend/src/modules/app/views/dashboard/Main.vue
+++ frontend/src/modules/app/views/dashboard/Main.vue
@@ -1,20 +1,19 @@
 <template lang="">
   <div class="grid grid-cols-4 grid-rows-4 h-full gap-8 grid-flow-col">
     <div class="grid grid-cols-4 grid-rows-4 h-full gap-8 grid-flow-row col-span-2 row-span-4">
-      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
-      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
-      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
-      <div class="bg-proto-lightgrey rounded-lg col-span-2"></div>
-      <div class="bg-proto-lightgrey rounded-lg col-span-4 row-span-2"></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2 p-4"><p class="font-h2">Neue Nachrichten</p></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2 p-4"><p class="font-h2">Cloud Updates</p></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2 p-4"><p class="font-h2">Registrierungsanfragen</p></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-2 p-4"><p class="font-h2">Neue Termine</p></div>
+      <div class="bg-proto-lightgrey rounded-lg col-span-4 row-span-2 p-4"><p class="font-h2">Termine</p></div>
     </div>
-    <div class="bg-proto-lightgrey rounded-lg col-span-2 row-span-4"></div>
+    <div class="bg-proto-lightgrey rounded-lg col-span-2 row-span-4 p-4"><p class="font-h2">Veranstaltungen</p></div>
   </div>
 </template>
+
+
 <script lang="ts">
 export default {
   
 }
 </script>
-<style lang="">
-  
-</style>
